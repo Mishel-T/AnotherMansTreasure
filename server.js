83347579c205c292a0dded7e404468bb811056d4
David <daveshuang@gmail.com>
2019-07-31 13:01:12 -0700

    Updated to es6 standard

--- server.js
+++ server.js
@@ -2,22 +2,25 @@ require("dotenv").config();
 const express = require("express");
 const exphbs = require("express-handlebars");
 
+// Requiring our models for syncing
 const db = require("./models");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
+// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+// Static directory
 app.use(express.static("public"));
 
 // Handlebars
 app.engine(
-  "handlebars",
-  exphbs({
-    defaultLayout: "main"
-  })
+    "handlebars",
+    exphbs({
+        defaultLayout: "main"
+    })
 );
 app.set("view engine", "handlebars");
 
@@ -30,18 +33,14 @@ const syncOptions = { force: false };
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
 if (process.env.NODE_ENV === "test") {
-  syncOptions.force = true;
+    syncOptions.force = true;
 }
 
 // Starting the server, syncing our models ------------------------------------/
 db.sequelize.sync(syncOptions).then(() => {
-  app.listen(PORT, () => {
-    console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
-  });
+    app.listen(PORT, () => {
+        console.log(`==> 🌎  Listening on port ${PORT}. Visit http://localhost:${PORT}/ in your browser.`);
+    });
 });
 
 module.exports = app;
