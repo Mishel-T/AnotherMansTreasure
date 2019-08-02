166def0489210fedf1b858ac0eee61af1cf0527b
David Huang <daveshuang@gmail.com>
2019-08-01 23:44:19 -0700

    modified routes to use static html files

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,36 @@
 const db = require("../models");
 
 module.exports = app => {
-  // Get all examples
-  app.get("/api/examples", (req, res) => {
-    db.Example.findAll({}).then(dbExamples => {
-      res.json(dbExamples);
+    // Get all examples
+    app.get("/api/examples", (req, res) => {
+        db.Example.findAll({})
+            .then(dbExamples => {
+                res.json(dbExamples);
+            });
+        // db.Example.findAll({
+        //     where: {
+        //         location: 'Riverside' //example
+        //     }
+        // }).then(dbExamples => {
+        //     res.json(dbExamples);
+        // });
     });
-  });
 
-  // Create a new example
-  app.post("/api/examples", (req, res) => {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+    // Create a new example
+    app.post("/api/examples", (req, res) => {
+        db.Example
+            .create(req.body)
+            .then(dbExample => {
+                res.json(dbExample);
+            });
     });
-  });
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", (req, res) => {
-    db.Example.destroy({ where: { id: req.params.id } }).then(dbExample => {
-      res.json(dbExample);
+    // Delete an example by id
+    app.delete("/api/examples/:id", (req, res) => {
+        db.Example
+            .destroy({ where: { id: req.params.id } })
+            .then(dbExample => {
+                res.json(dbExample);
+            });
     });
-  });
 };
