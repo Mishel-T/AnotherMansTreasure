9e66d0ec0db0738ca09ac021b02b89cc0b205be0
Mark Pahor <mark@Marks-MBP.socal.rr.com>
2019-08-04 23:50:29 -0700

    styles done, minor issues to fix

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,14 @@
-//create on click even for login button. On login, capture id from db based on username
+//category dropdown initialize
+$('.dropdown-trigger').dropdown();
+$('#dropdown1 li').click(function () {
+    a = $(this).text()
+    $('.dropdownBtn').text(a)
+})
+
+//create on click even for login button. On login, capture id from db based on username - would actually go in index.js
 
 //show password toggle on 
-$("#pwshow").click(function() {
+$("#pwshow").click(function () {
     var x = document.getElementById("password");
     if (x.type === "password") {
         x.type = "text";
